31298d979104c0afaa78a6189e6625dd40d9b518
swarnim-dhairya <83215601+swarnim-dhairya@users.noreply.github.com>
2021-10-21 22:25:36 +0530

    update view studet for dynamic data

--- view-students.php
+++ view-students.php
@@ -3,38 +3,47 @@ $data = file_get_contents ('studentData.json');
 $json = json_decode($data, true);
 $records = array();
 foreach ($json as $key => $value) {
-    if (!is_array($value)) {
+    
        $records[$key] = $value ;
-    } else {
-        foreach ($value as $key => $val) {
-          $records[$key] =  $val;
-        }
-    }
+   
 }
 echo "<pre>";
-print_r($records);
+//print_r($records);die;
 ?>
-<table class="studentInfo">
+<style type="text/css">
+        .studentInfo {
+            width: 500px;
+            border: 1px solid black;
+        }
+    </style>
+<table class="studentInfo" >
         <tbody><tr>
-            <td>Name</td>
-            <td>Father name</td>
-            <td>Email</td>
-            <td>Address</td>
-            <td>Dob</td>
-            <td>Gender</td>
-            <td>photo1</td>
-            <td>photo2</td>
+            <td><b>Fname</b></td>
+            <td><b>Lname</b></td>
+            <td><b>Dob</b></td>
+            <td><b>Fathername</b></td>
+            <td><b>Country</b></td>
+            <td><b>Gender</b></td>
+            <td><b>Email</b></td>
+            <td><b>Mob</b></td>
+            <td><b>Address</b></td>
+            <td><b>Photo</b></td>
         </tr>
-     
+        <?php foreach($records as $key=>$val) {?>
         <tr>
-            <td><?php echo $rec['fname'] $rec['fname']?></td>
-            <td><?php echo $rec['fname'] $rec['fname']?></td>
-            <td>87</td>
-            <td>88</td>
-            <td>92</td>
-            <td>88</td>
+            <td><?php if(isset($val['fname'])) { echo $val['fname']; }?></td>
+            <td><?php if(isset($val['lname'])) { echo $val['lname']; }?></td>
+            <td><?php if(isset($val['dob'])) { echo $val['dob']; }?></td>
+            <td><?php if(isset($val['Fathername'])) { echo $val['Fathername']; }?></td>
+            <td><?php if(isset($val['country'])) { echo $val['country']; }?></td>
+            <td><?php if(isset($val['gender'])) { echo $val['gender']; }?></td>
+            <td><?php if(isset($val['email'])) { echo $val['email']; }?></td>
+            <td><?php if(isset($val['mob'])) { echo $val['mob']; }?></td>
+            <td><?php if(isset($val['address'])) { echo $val['address']; }?></td>
+            <td><?php if( !empty($val['photo'])) { ?> <img  width="80" height="80" src=<?php echo $val['photo']; ?>> <?php } ?></td>
+
         </tr>
-     
+        <?php } ?>
        
-       
+      
     </tbody></table>
